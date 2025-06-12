a84559a111a5e8742eb50c71605774e4354c7c50
bblair321 <bblair321@yahoo.com>
2025-06-12 14:17:25 -0700

    possible fix for text not displaying

--- client.lua
+++ client.lua
@@ -1,19 +1,38 @@
 Citizen.CreateThread(function()
     while true do
-        Citizen.Wait(1000)
+        Citizen.Wait(0)
 
         local playerPed = PlayerPedId()
         local coords = GetEntityCoords(playerPed)
 
         local streetHash1, streetHash2 = GetStreetNameAtCoord(coords.x, coords.y, coords.z)
-        local streetName1 = GetStreetNameFromHashKey(streetHash1)
-        local streetName2 = GetStreetNameFromHashKey(streetHash2)
+        local streetName1 = GetStreetNameFromHashKey(streetHash1) or "Unknown"
+        local streetName2 = GetStreetNameFromHashKey(streetHash2) or ""
 
-        local displayText = streetName1 or "Unknown"
-        if streetName2 and streetName2 ~= "" then
+        local displayText = streetName1
+        if streetName2 ~= "" then
             displayText = displayText .. " & " .. streetName2
         end
 
-        print("[StreetNames] " .. displayText)
+        -- Draw background rectangle (black, semi-transparent)
+        local rectX = 0.5
+        local rectY = 0.03
+        local rectWidth = 0.3
+        local rectHeight = 0.03
+        DrawRect(rectX, rectY, rectWidth, rectHeight, 0, 0, 0, 150)
+
+        -- Draw the street name text (yellow)
+        SetTextFont(4)
+        SetTextProportional(1)
+        SetTextScale(0.6, 0.6)
+        SetTextColour(255, 255, 0, 255) -- bright yellow
+        SetTextDropshadow(0, 0, 0, 0, 255)
+        SetTextEdge(1, 0, 0, 0, 255)
+        SetTextDropShadow()
+        SetTextOutline()
+        SetTextEntry("STRING")
+        AddTextComponentString(displayText)
+        -- Center the text horizontally at 0.5, vertically at rectY - half rect height
+        DrawText(rectX - (string.len(displayText) * 0.0035), rectY - (rectHeight / 2) + 0.005)
     end
 end)
